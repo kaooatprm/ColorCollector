52c540f2cfa2bac6f83742c3a4165bf61819c7e0
XSectorZ <panatschool@gmail.com>
2025-02-08 05:28:44 +0700

    Color finder with blindsearch algo

--- colorcollector/src/app/page.tsx
+++ colorcollector/src/app/page.tsx
@@ -9,6 +9,22 @@ interface Performance {
 
 }
 
+interface ColorObject {
+  colorType: string;
+  colorPos: number[];
+  flagColorPos: number[];
+}
+
+const mockData = [{
+  colorType: "#DB1D1D",
+  colorPos: [8,35],
+  flagColorPos: [17,35]
+},{
+  colorType: "#10DA60",
+  colorPos: [1,10],
+  flagColorPos: [25,55]
+}]
+
 export default function Home() {
   const numRows = 30;
   const numCols = 80;
@@ -17,6 +33,9 @@ export default function Home() {
   const [selectState, setSelectState] = useState<string>("WALL");
   const [startPos, setStartPos] = useState<[number, number]>([13, 20]);
   const [endPos, setEndPos] = useState<[number, number]>([13, 50]);
+
+  const [colorPos, setColorPos] = useState<ColorObject[]>(mockData);
+
   const [isPlaying,setIsPlaying] = useState<boolean>(false);
   const [blindSearchPerformance, setBlindSearchPerformance] = useState<Performance>({runTime: 0, memUsage: 0});
   const [heuristicPerformance, setHeuristicSearchPerformance] = useState<Performance>({runTime: 0, memUsage: 0});
@@ -30,6 +49,12 @@ export default function Home() {
     if(isPlaying) return;
     const newGrid = [...grid];
     clearBoard();
+
+    const isColorPosMatch = colorPos.some(({ colorPos }) => colorPos[0] === row && colorPos[1] === col);
+    const isFlagColorPosMatch = colorPos.some(({ flagColorPos }) => flagColorPos[0] === row && flagColorPos[1] === col);
+  
+    if (isColorPosMatch || isFlagColorPosMatch) return;
+  
     if (selectState === "WALL") {
       if (row === endPos[0] && col === endPos[1]) return;
       if (row === startPos[0] && col === startPos[1]) return;
@@ -52,6 +77,11 @@ export default function Home() {
 
   const handleMouseEnter = (row: number, col: number) => {
     if(isPlaying) return;
+
+    const isColorPosMatch = colorPos.some(({ colorPos }) => colorPos[0] === row && colorPos[1] === col);
+    const isFlagColorPosMatch = colorPos.some(({ flagColorPos }) => flagColorPos[0] === row && flagColorPos[1] === col);
+  
+    if (isColorPosMatch || isFlagColorPosMatch) return;
     if (!isMouseDown || selectState !== "WALL") return;
     if (row === endPos[0] && col === endPos[1]) return;
     if (row === startPos[0] && col === startPos[1]) return;
@@ -97,78 +127,181 @@ export default function Home() {
     const directions = [
       [0, 1], [1, 0], [0, -1], [-1, 0], // 4 ทิศทาง: ขวา, ล่าง, ซ้าย, ขึ้น
     ];
-    const queue: Array<[number, number]> = [startPos];
-    const visited = Array.from({ length: numRows }, () => Array(numCols).fill(false));
-    const parent: Array<Array<[number, number] | null>> = Array.from({ length: numRows }, () =>
+
+    let queue: Array<[number, number]> = [startPos];
+    let visited = Array.from({ length: numRows }, () => Array(numCols).fill(false));
+    let parent: Array<Array<[number, number] | null>> = Array.from({ length: numRows }, () =>
       Array(numCols).fill(null)
     );
-  
     visited[startPos[0]][startPos[1]] = true;
     newGrid[startPos[0]][startPos[1]] = 10;
+
     let queueIndex = 0;
     let foundEnd = false;
+
+    const resetBoardForColorFinding = (startRow: number,startCol: number) => {
+
+      newGrid.map((row,rowindex) => {
+        newGrid[rowindex].map((col,colindex) => {
   
-    const processStep = () => {
+          if(newGrid[rowindex][colindex] !== 1) {
+            newGrid[rowindex][colindex] = 0;
+          }
+        })
+      }) 
+      queue = [[startRow,startCol]];
+      queueIndex = 0;
+      visited = Array.from({ length: numRows }, () => Array(numCols).fill(false));
+      parent = Array.from({ length: numRows }, () => Array(numCols).fill(null));
+      visited[startRow][startCol] = true;
+      newGrid[startRow][startCol] = 10;
+      setGrid([...newGrid]);
+    }
 
-      const windowPerformance = window.performance as any;
-      totalMemory = (totalMemory + windowPerformance.memory.usedJSHeapSize)/2;
-
-      console.log(totalMemory);
-
-      if (queueIndex >= queue.length || foundEnd) {
-        const endTime = performance.now();
-        console.log(`Runtime: ${endTime - startTime} ms`);
-        setBlindSearchPerformance({
-          runTime: endTime - startTime,
-          memUsage: totalMemory
-        })  
-        setIsPlaying(false);
-        return;
-      }
+    if(colorPos) {
+      console.log("Come");
+      let colorFinderSize = colorPos.length;
+      let currentColorFinderIndex = 0;
+      let currentColorFinderState = 0; // 0 = find color, 1 = find hole
+    
+      const processStep = () => {
+        console.log("Search....")
+        if(queueIndex >= queue.length || foundEnd) {
+          const endTime = performance.now();
+          console.log("queueIndex",queueIndex)
+          console.log("queue.length",queue.length)
+          console.log(`Runtime: ${endTime - startTime} ms`);
+          setBlindSearchPerformance({
+            runTime: endTime - startTime,
+            memUsage: totalMemory
+          })  
+          setIsPlaying(false);
+          return;
+        }
+    
+        const [row, col] = queue[queueIndex];
+        queueIndex++;
+    
+        for (let [dx, dy] of directions) {
+          const newRow = row + dx;
+          const newCol = col + dy;
+    
+          if (
+            newRow >= 0 &&
+            newRow < numRows &&
+            newCol >= 0 &&
+            newCol < numCols &&
+            !visited[newRow][newCol] &&
+            grid[newRow][newCol] !== 1
+          ) {
+            visited[newRow][newCol] = true;
+            queue.push([newRow, newCol]);
+            parent[newRow][newCol] = [row, col];
+            newGrid[newRow][newCol] = 10;
+    
+            if(colorFinderSize > 0) {
+              if(currentColorFinderState === 0 && colorPos[currentColorFinderIndex].colorPos[0] === newRow && colorPos[currentColorFinderIndex].colorPos[1] === newCol) {
+                console.log("Found Color Index",currentColorFinderIndex);
+                currentColorFinderState = 1;
+                resetBoardForColorFinding(colorPos[currentColorFinderIndex].colorPos[0], colorPos[currentColorFinderIndex].colorPos[1]);
+              } else if(currentColorFinderState === 1 && colorPos[currentColorFinderIndex].flagColorPos[0] === newRow && colorPos[currentColorFinderIndex].flagColorPos[1] === newCol) {
+                console.log("Found Color Flag Index",currentColorFinderIndex);
+                currentColorFinderState = 0;
+                resetBoardForColorFinding(colorPos[currentColorFinderIndex].flagColorPos[0], colorPos[currentColorFinderIndex].flagColorPos[1]);
+                colorFinderSize--;
+                currentColorFinderIndex+=1;
+              }
+            } else {
+              if (newRow === endPos[0] && newCol === endPos[1]) {
+                console.log("Found! NA");
+                foundEnd = true;
+                const endTime = performance.now();
+                console.log(`Runtime: ${endTime - startTime} ms`);
+    
+                setBlindSearchPerformance({
+                  runTime: endTime - startTime,
+                  memUsage: totalMemory
+                })  
+                setIsPlaying(false);
+                foundEnd = true;
+                setGrid([...newGrid]);
+                return;
+              }
+            }
+          }   
+        }
+    
+        // เรียก requestAnimationFrame เพื่อให้ processStep ทำงานในเฟรมถัดไป
+        if (!foundEnd) {
+          const ID = requestAnimationFrame(processStep);
+          setAnimationFrameId(ID); 
+        }
+      };
+    
+      // เริ่มต้นกระบวนการด้วย requestAnimationFrame
+      const ID = requestAnimationFrame(processStep);
+      setAnimationFrameId(ID);
+    } else {
+      const processStep = () => {
   
-      const [row, col] = queue[queueIndex];
-      queueIndex++;
-
-      for (let [dx, dy] of directions) {
-        const newRow = row + dx;
-        const newCol = col + dy;
+        const windowPerformance = window.performance as any;
+        totalMemory = (totalMemory + windowPerformance.memory.usedJSHeapSize)/2;
   
-        if (
-          newRow >= 0 &&
-          newRow < numRows &&
-          newCol >= 0 &&
-          newCol < numCols &&
-          !visited[newRow][newCol] &&
-          grid[newRow][newCol] !== 1
-        ) {
-          visited[newRow][newCol] = true;
-          queue.push([newRow, newCol]);
-          parent[newRow][newCol] = [row, col];
-          newGrid[newRow][newCol] = 10;
-          if (newRow === endPos[0] && newCol === endPos[1]) {
-            console.log("Found! NA");
-            const endTime = performance.now();
-            console.log(`Runtime: ${endTime - startTime} ms`);
-
-            setBlindSearchPerformance({
-              runTime: endTime - startTime,
-              memUsage: totalMemory
-            })  
-            setIsPlaying(false);
-            foundEnd = true;
-            setGrid([...newGrid]);
-            return;
+        if (queueIndex >= queue.length || foundEnd) {
+          const endTime = performance.now();
+          console.log(`Runtime: ${endTime - startTime} ms`);
+          setBlindSearchPerformance({
+            runTime: endTime - startTime,
+            memUsage: totalMemory
+          })  
+          setIsPlaying(false);
+          return;
+        }
+    
+        const [row, col] = queue[queueIndex];
+        queueIndex++;
+  
+        for (let [dx, dy] of directions) {
+          const newRow = row + dx;
+          const newCol = col + dy;
+    
+          if (
+            newRow >= 0 &&
+            newRow < numRows &&
+            newCol >= 0 &&
+            newCol < numCols &&
+            !visited[newRow][newCol] &&
+            grid[newRow][newCol] !== 1
+          ) {
+            visited[newRow][newCol] = true;
+            queue.push([newRow, newCol]);
+            parent[newRow][newCol] = [row, col];
+            newGrid[newRow][newCol] = 10;
+            if (newRow === endPos[0] && newCol === endPos[1]) {
+              console.log("Found! NA");
+              const endTime = performance.now();
+              console.log(`Runtime: ${endTime - startTime} ms`);
+  
+              setBlindSearchPerformance({
+                runTime: endTime - startTime,
+                memUsage: totalMemory
+              })  
+              setIsPlaying(false);
+              foundEnd = true;
+              setGrid([...newGrid]);
+              return;
+            }
           }
         }
-      }
-  
-      setGrid([...newGrid]);
-      const ID = requestAnimationFrame(processStep); 
-      setAnimationFrameId(ID);
-    };
-  
-    const ID = requestAnimationFrame(processStep);
-    setAnimationFrameId(ID);
+    
+        setGrid([...newGrid]);
+        const ID = requestAnimationFrame(processStep); 
+        setAnimationFrameId(ID);
+      };
+    
+      const ID = requestAnimationFrame(processStep);
+      setAnimationFrameId(ID); 
+    }
   };
   
   
@@ -278,7 +411,12 @@ export default function Home() {
   
 
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex relative flex-col w-full h-full">
+      <div className={`bg-[#c5c5c5] pt-1 px-1 items-center absolute bottom-[20%] w-28 h-[500px] ml-3 ${selectState === "COLOR" ? 'flex flex-col' : 'hidden'}`}>
+          <div className="flex">
+              Pick Color
+          </div>
+      </div>
       <div className="flex justify-center items-center w-full">HEADER</div>
       <div className="flex gap-x-5 flex-row mt-5 justify-center items-center">
         <div
@@ -314,6 +452,17 @@ export default function Home() {
           </div>
           <div className="flex justify-center items-center">End</div>
         </div>
+        <div
+          className={`flex flex-row px-2 py-1 rounded-md border border-gray-600 ${
+            selectState == "COLOR" ? " bg-gray-300" : "cursor-pointer hover:bg-gray-200"
+          }`}
+          onClick={() => handleClickChangeState("COLOR")}
+        >
+          <div className="flex justify-center items-center mr-1">
+            <Icon icon="ic:baseline-color-lens" width={20} height={20} />
+          </div>
+          <div className="flex justify-center items-center">Color Collector</div>
+        </div>
       </div>
       <div className="flex flex-row gap-x-5 mt-5 w-full justify-center">
           <div className="flex px-2 py-1 rounded-md text-white cursor-pointer bg-green-600 hover:bg-green-500" onClick={handleFindPath}>
@@ -323,9 +472,9 @@ export default function Home() {
             Heuristic Search
           </div>
       </div>
-      <div className="flex mt-12 justify-center items-center w-full">
+      <div className="flex mt-5 justify-center items-center w-full">
         <table
-          className="border border-collapse border-black w-full"
+          className="border border-collapse border-black w-[85%]"
           onMouseUp={() => setIsMouseDown(false)}
         >
           <tbody>
@@ -362,6 +511,38 @@ export default function Home() {
                         />
                       </div>
                     )}
+                    {(() => {
+                      const matchedFlag = colorPos.find(({ colorPos }) => 
+                        colorPos[0] === rowIndex && colorPos[1] === colIndex
+                      );
+                      return matchedFlag ? (
+                        <div className="flex w-full h-full justify-center items-center">
+                          <Icon
+                            icon="material-symbols:circle"
+                            width={20}
+                            height={20}
+                            style={{ color: matchedFlag.colorType }}
+                          />
+                        </div>
+                      ) : null;
+                    })()}
+                    {(() => {
+                      const matchedFlag = colorPos.find(({ flagColorPos }) => 
+                        flagColorPos[0] === rowIndex && flagColorPos[1] === colIndex
+                      );
+                      return matchedFlag ? (
+                        <div className="flex w-full h-full justify-center items-center">
+                          <Icon
+                            icon="material-symbols:flag-rounded"
+                            width={20}
+                            height={20}
+                            style={{ color: matchedFlag.colorType }}
+                          />
+                        </div>
+                      ) : null;
+                    })()}
+
+
                   </td>
                 ))}
               </tr>
